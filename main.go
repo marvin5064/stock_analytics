1f2431011b0e526cba6b74ecd25db745c9791eb7
marvin cai <marvin5064@gmail.com>
2018-04-22 23:34:13 +0800

    version control

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/parnurzeal/gorequest"
+)
 
 func main() {
+	resp, body, errs := gorequest.New().Get("http://www.google.com").End()
+	if errs != nil {
+		fmt.Println(errs)
+	}
+	fmt.Println(resp, body)
 	fmt.Println("init")
 }
