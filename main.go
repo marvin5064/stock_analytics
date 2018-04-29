1c5e451d43045d21a0462b7079fc48f581e3a2f6
marvin cai <marvin5064@gmail.com>
2018-04-29 22:20:46 +0800

    set up logs and config loading

--- main.go
+++ main.go
@@ -1,20 +1,33 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/marvin5064/stock-analytics/lib/logger"
 	"github.com/parnurzeal/gorequest"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	defer logger.Sync()
+
+	viper.SetConfigType("json")
+	viper.AddConfigPath("./config/")
+	err := viper.ReadInConfig()
+	if err != nil {
+		logger.Fatal("fail to load config")
+	}
+
+	apikey := viper.GetString("apikey")
+	if apikey != "" {
+		logger.Info("successfully loaded api key from config")
+	}
 	resp, body, errs := gorequest.New().
 		Get("https://www.alphavantage.co/query").
 		Param("function", "TIME_SERIES_DAILY").
 		Param("symbol", "0941.hk").
-		Param("apikey", "H6I7QGO4NATSFBXQ").
+		Param("apikey", apikey).
 		End()
 	if errs != nil {
-		fmt.Println(errs)
+		logger.Error(errs)
 	}
-	fmt.Println(resp, body)
+	logger.Info(resp, body)
 }
